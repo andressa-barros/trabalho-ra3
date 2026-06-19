1d1f089b822923b0e7dc0f23d5377c66d72928f3
Lissa Deguti <lilideguti@gmail.com>
2026-06-19 14:28:43 -0300

    chore: Só arrumando os comentários pra ficar mais fácil de entender depois

--- programa1.py
+++ programa1.py
@@ -1,16 +1,16 @@
 from itertools import combinations
 
 def gerar_combinacoes(n, p):
-    """
-    Gera combinações de p elementos a partir de um universo de n elementos.
-    Utiliza um gerador (yield) para economizar memória RAM.
-    """
+    # gera combinações de "p" elementos a partir de um universo de "n" elementos
+    # utiliza um gerador (yield) para economizar memória RAM.
+
     universo = range(1, n + 1)
+
     # combinations retorna um iterador otimizado em C
     return combinations(universo, p)
 
 def imprimir_cardinalidade(p):
-    # apenas para validar se a cardinalidade bate com o enunciado [cite: 16]
+    # valida se a cardinalidade bate com o enunciado
     n = 25
     import math
     card = math.comb(n, p)
