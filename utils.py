c7b6eb0ece4f1311a90c459a2607335bbd439429
Lissa Deguti <lilideguti@gmail.com>
2026-06-19 14:53:54 -0300

    chore: Arrumando comentários

--- utils.py
+++ utils.py
@@ -3,11 +3,11 @@ import sys
 from itertools import combinations
 
 def gerar_combinacoes(n, p):
-    # Gera combinações usando iterador (otimizado em C)
+    # gera combinações usando iterador (otimizado em C)
     return combinations(range(1, n + 1), p)
 
 def estimar_memoria_materializada(n, p):
-    # Estima RAM necessária se usássemos uma lista
+    # estima RAM necessária se usássemos uma lista
     qtd = math.comb(n, p)
     amostra = tuple(range(p))
     tamanho_tupla = sys.getsizeof(amostra) + sum(sys.getsizeof(x) for x in amostra)
